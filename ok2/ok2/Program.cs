4b5d6c2ffe25cdb088ca61684e0337672384ba3a
Nanban-Ship <adamtybl0@gmail.com>
2025-09-15 09:57:39 +0200

    Add files via upload
    nice one

---
+++ ok2/ok2/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ok2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int width = int.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+            char[,] labyrinth = new char[height, width];
+                
+            for (int x = 0; x < height; x++) {
+                    string line = Console.ReadLine();
+                for (int y = 0; y < width; y++)
+                {
+                    labyrinth[x, y] = line[y];
+                }
+            }
+        }        
+        }
+    }
+ 
+
