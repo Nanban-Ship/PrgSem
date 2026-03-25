570aecea0bf02b184da36b11d43ad19ae5479137
Nanban-Ship <adamtybl0@gmail.com>
2026-03-25 23:20:11 +0100

    todolist

---
+++ TODOlist/TODOlist/Model/TODOlist.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace TODOlist.Model
+{
+    public class TodoTask : INotifyPropertyChanged
+    {
+        private string _text;
+        private DateTime _deadline;
+
+        public string Text
+        {
+            get => _text;
+            set { _text = value; OnPropertyChanged(); }
+        }
+
+        public DateTime Deadline
+        {
+            get => _deadline;
+            set
+            {   _deadline = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(IsOverdue));
+            }
+        }
+        public bool IsOverdue => Deadline.Date < DateTime.Now.Date;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
---
+++ TODOlist/TODOlist/ViewModel/MainViewModel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using TODOlist.Model;
+
+namespace TODOlist.ViewModel
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public ObservableCollection<TodoTask> Tasks { get; set; } = new ObservableCollection<TodoTask>();
+
+        private TodoTask _selectedTask;
+        public TodoTask SelectedTask
+        {
+            get => _selectedTask;
+            set { _selectedTask = value; OnPropertyChanged(); }
+        }
+
+        private string _newTaskText;
+        public string NewTaskText
+        {
+            get => _newTaskText;
+            set { _newTaskText = value; OnPropertyChanged(); }
+        }
+
+        private DateTime _newTaskDeadline = DateTime.Now;
+        public DateTime NewTaskDeadline
+        {
+            get => _newTaskDeadline;
+            set { _newTaskDeadline = value; OnPropertyChanged(); }
+        }
+
+        public ICommand AddCommand { get; }
+        public ICommand RemoveCommand { get; }
+
+        public MainViewModel()
+        {
+            AddCommand = new RelayCommand(
+                _ => {
+                    Tasks.Add(new TodoTask { Text = NewTaskText, Deadline = NewTaskDeadline });
+                    NewTaskText = string.Empty;
+                },
+                _ => !string.IsNullOrWhiteSpace(NewTaskText)
+            );
+            RemoveCommand = new RelayCommand(
+                _ => Tasks.Remove(SelectedTask),
+                _ => SelectedTask != null
+            );
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}   
